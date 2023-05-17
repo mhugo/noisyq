1ce09dd6d6036a3a32f6051f71cb6072a6834b37
Hugo Mercier <hugo@ternet.fr>
2023-05-17 22:27:51 +0200

    Remove unused files

--- experiments/test_osc.py
+++
@@ -1,9 +0,0 @@
-from pythonosc.udp_client import SimpleUDPClient
-
-ip = "127.0.0.1"
-port = 10679
-
-client = SimpleUDPClient(ip, port)  # Create client
-
-#client.send_message("/Carla_Rack/set_parameter_value", [56, 0.5])   # Send float message
-client.send_message("/Carla_Rack/0/set_parameter_value", [59, 0.5])   # Send float message
